e23f53d011f78c7dfedab347fe904a32d36193e0
vitaliynester <36518409+vitaliynester@users.noreply.github.com>
2020-07-13 20:20:34 +0300

    Update config.py

--- Backend/config.py
+++ Backend/config.py
@@ -1,3 +1,3 @@
 class Config:
-    SECRET_KEY = '9de2d9d7e3b6470db90f214077cc27b5'
-    SQLACHEMY_DATABASE_URI = 'postgresql://postgres:uhibim22@localhost/carshowroom'
+    SECRET_KEY = ''
+    SQLACHEMY_DATABASE_URI = ''
